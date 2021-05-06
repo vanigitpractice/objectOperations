2c6f41b296a3dc329d95521755b4890f3f3d5f6c
sures <sures@LAPTOP-4M6LAJK0>
2021-05-06 11:06:36 -0400

    this is updatting

---
+++ webDriverPractice/src/MyWebDriver/TestGitClass.java
@@ -0,0 +1,11 @@
+package MyWebDriver;
+
+public class TestGitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add new class");
+
+	}
+
+}
